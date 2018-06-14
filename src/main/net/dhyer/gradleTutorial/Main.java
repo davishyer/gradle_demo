1f964a16b9d556bb4ac595b0a203d8734f4a53c8
Davis Hyer <dhyer@instructure.com>
2018-06-13 19:40:17 -0600

    gradle demo

---
+++ src/main/net/dhyer/gradleTutorial/Main.java
@@ -0,0 +1,13 @@
+package net.dhyer.gradleTutorial;
+
+import java.util.Arrays;
+// PART 2 - Dependencies
+// import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
+    // PART 2 - Dependencies
+    // System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+  }
+}
